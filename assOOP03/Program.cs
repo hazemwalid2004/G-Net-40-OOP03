009ce8f42a6ee11c4f2dd92f883890a14951f895
Hazem walid <walidf106@gmail.com>
2026-02-24 02:34:32 +0200

    Q1_part1

--- assOOP03/Program.cs
+++ assOOP03/Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            #region Q1_part1
+            //a) A University has Departments. If the university is closed, the departments no longer exist.
+            //Composition
+            //b) A Driver uses a Car. The driver does not own the car.
+            // Association
+            //c) A Dog is an Animal.
+            //Inheritance
+            //d) A Team has Players. If the team is deleted, the players still exist.
+            //Aggregation
+            //e) A method receives a Logger as a parameter and calls it inside the method only.
+            //Dependency
+            #endregion
         }
     }
 }
