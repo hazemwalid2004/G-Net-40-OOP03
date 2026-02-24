5d97223f886ce7acd88c1c946270bdf37beadce3
Hazem walid <walidf106@gmail.com>
2026-02-24 02:42:32 +0200

    Q2_Part1

--- assOOP03/Program.cs
+++ assOOP03/Program.cs
@@ -17,6 +17,16 @@
             //e) A method receives a Logger as a parameter and calls it inside the method only.
             //Dependency
             #endregion
+            #region Q2_Part1
+            //a) A parent class has a protected field. Can a child class in a different assembly access it? What about through an object instance from outside?
+            //yes / can't access
+            //b) What is the difference between protected internal and private protected?
+            //protected internal -> can access inside and outside assembly/private protected-> can access inside only assembly 
+            //c) What does the sealed keyword do when applied to a class? What about when applied to a method?
+            //we can't inhert from this class /we can't override the method
+            //d) Can you create an object from a sealed class using new? Why or why not?
+            //yes we can but we can't inhert from it 
+            #endregion
         }
     }
 }
